89c75074cf7798480201cc383c3c4d3f8ae73167
Gene Edwards <eedwards@su.edu>
2020-02-14 12:13:39 -0500

    Changed password

--- empTracker.js
+++ empTracker.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "elonda2eugene",
+  password: "testpassword",
   database: "empTracker_DB"
 });
 
